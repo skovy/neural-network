a27973baebb6b7a7880c0ec08ab43be844545e9f
Spencer Miskoviak <smiskoviak@gmail.com>
2017-01-20 08:18:03 -0500

    :art: cleanup the main file and :pencil2: add a few comments for clairty

--- main.py
+++ main.py
@@ -12,29 +12,39 @@ from network import Network
 # from the input and the bias input
 n = Network(2, [0.1, 0.2, 0.3])
 
-# the training set to learn the OR function
-training_set = [
+# a simple training set to learn the OR function
+or_function_training_set = [
   { 'inputs': [0, 0], 'expected_output': 0 },
   { 'inputs': [0, 1], 'expected_output': 1 },
   { 'inputs': [1, 0], 'expected_output': 1 },
   { 'inputs': [1, 1], 'expected_output': 1 }
 ]
 
-n.train(training_set)
+# a simple training set to learn the AND function
+and_function_training_set = [
+  { 'inputs': [0, 0], 'expected_output': 0 },
+  { 'inputs': [0, 1], 'expected_output': 0 },
+  { 'inputs': [1, 0], 'expected_output': 0 },
+  { 'inputs': [1, 1], 'expected_output': 1 }
+]
 
+# perform training with the provided training set
+n.train(and_function_training_set)
 n.final_weights()
 
+# all (x, y) pairs that produce a positive output
 positive_x = []
 positive_y = []
 
+# all (x, y) pairs that produce a negative output
 negative_x = []
 negative_y = []
 
 steps = 10 # number of steps on each axis
-start_pos = -1 * steps # the x, y mins
-end_pos = 2 * steps # the x,y max
+start_pos = -1 * steps # the x and y mins
+end_pos = 2 * steps # the x and y max
 
-# generate a "matrix" with much finer steps to "brute-force chart"" the resulting function
+# generate a "matrix" with much finer steps to "brute-force chart" the resulting function
 for i in range(start_pos, end_pos):
   for j in range(start_pos, end_pos):
     x = i / float(steps)
@@ -46,7 +56,8 @@ for i in range(start_pos, end_pos):
       negative_x.append(x)
       negative_y.append(y)
 
-# Create traces
+# create traces of the positive and negative outputs
+# and generate a scatter plot of the resulting data
 positive_trace = go.Scatter(
     x = positive_x,
     y = positive_y,
@@ -56,7 +67,6 @@ positive_trace = go.Scatter(
         color = '#06D6A0'
     )
 )
-
 negative_trace = go.Scatter(
     x = negative_x,
     y = negative_y,
@@ -66,9 +76,5 @@ negative_trace = go.Scatter(
         color = '#EF476F'
     )
 )
-
-
 data = [positive_trace, negative_trace]
-
-# Plot and embed in ipython notebook!
-py.offline.plot(data, filename='or-scatter.html')
+py.offline.plot(data, filename='scatter.html')
