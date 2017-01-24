69edce3ee9f2129514ca98136cc05de2eb66bf2a
Spencer Miskoviak <smiskoviak@gmail.com>
2017-01-23 20:27:50 -0500

    conditionally show visualizations

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import sys
+
 # data manipulation and helpers
 import numpy as np
 
-# graphing helpers
+# graphing helpers from Plotyly
 import plotly as py
 import plotly.graph_objs as go
 
-# the neural network
+# the custom neural network
 from network import Network
+
+if len(sys.argv) != 2:
+  raise Exception("Incorrect parameters. Correct usage: python main.py <visualization:boolean>")
+
+show_visualization = (sys.argv[1] == "true")
 
 # create a network with 2 inputs and the initial weights for the connections
 # from the input and the bias input
-n = Network(2, [0.1, 0.2, 0.3])
+n = Network(2, [1, 1])
 
 # a simple training set to learn the OR function
 or_function_training_set = [
@@ -32,51 +39,52 @@ and_function_training_set = [
 n.train(or_function_training_set)
 n.final_weights()
 
-# all (x, y) pairs that produce a positive output
-positive_x = []
-positive_y = []
+if show_visualization:
+  # all (x, y) pairs that produce a positive output
+  positive_x = []
+  positive_y = []
 
-# all (x, y) pairs that produce a negative output
-negative_x = []
-negative_y = []
+  # all (x, y) pairs that produce a negative output
+  negative_x = []
+  negative_y = []
 
-steps = 5 # number of steps on each axis
-start_pos = -1 * steps # the x and y mins
-end_pos = 2 * steps # the x and y max
+  steps = 5 # number of steps on each axis
+  start_pos = -1 * steps # the x and y mins
+  end_pos = 2 * steps # the x and y max
 
-# generate a "matrix" with much finer steps to "brute-force chart" the resulting function
-for i in range(start_pos, end_pos):
-  for j in range(start_pos, end_pos):
-    x = i / float(steps)
-    y = j / float(steps)
-    if n.run_single_input([x, y]):
-      positive_x.append(x)
-      positive_y.append(y)
-    else:
-      negative_x.append(x)
-      negative_y.append(y)
+  # generate a "matrix" with much finer steps to "brute-force chart" the resulting function
+  for i in range(start_pos, end_pos):
+    for j in range(start_pos, end_pos):
+      x = i / float(steps)
+      y = j / float(steps)
+      if n.run_single_input([x, y]):
+        positive_x.append(x)
+        positive_y.append(y)
+      else:
+        negative_x.append(x)
+        negative_y.append(y)
 
-# create traces of the positive and negative outputs
-# and generate a scatter plot of the resulting data
-positive_trace = go.Scatter(
-    name = 'Positive Output',
-    x = positive_x,
-    y = positive_y,
-    mode = 'markers',
-    marker = dict(
-        size = 4,
-        color = '#06D6A0'
-    )
-)
-negative_trace = go.Scatter(
-    name = 'Negative Output',
-    x = negative_x,
-    y = negative_y,
-    mode = 'markers',
-    marker = dict(
-        size = 4,
-        color = '#EF476F'
-    )
-)
-data = [positive_trace, negative_trace]
-py.offline.plot(data, filename='scatter.html')
+  # create traces of the positive and negative outputs
+  # and generate a scatter plot of the resulting data
+  positive_trace = go.Scatter(
+      name = 'Positive Output',
+      x = positive_x,
+      y = positive_y,
+      mode = 'markers',
+      marker = dict(
+          size = 4,
+          color = '#06D6A0'
+      )
+  )
+  negative_trace = go.Scatter(
+      name = 'Negative Output',
+      x = negative_x,
+      y = negative_y,
+      mode = 'markers',
+      marker = dict(
+          size = 4,
+          color = '#EF476F'
+      )
+  )
+  data = [positive_trace, negative_trace]
+  py.offline.plot(data, filename='scatter.html')
